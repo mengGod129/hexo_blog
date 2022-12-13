d38c7b6094813b84d9b13cf51f4f74f13d062b74
Meng <meng@MengdeMacBook-Pro.local>
2022-12-13 20:42:42 +0800

    Site updated: 2022-12-13 20:42:42

--- javascripts/waline.js
+++ javascripts/waline.js
@@ -1,9 +1,12 @@
 import {
 	init
-} from 'https://unpkg.com/@waline/client@v2/dist/waline.mjs';
+} from 'https://cdn.jsdelivr.net/gh/mengGods/Blog_CDN_2@1.9.5/meng.me/waline.mjs';
 
 init({
 	el: '#waline-container',
 	serverURL: window.aomori.waline.serverURL,
-	search: window.aomori.waline.search
+	search: window.aomori.waline.search,
+	emoji: [
+      '//cdn.jsdelivr.net/gh/walinejs/emojis@1.1.0/alus',
+    ],
 });
